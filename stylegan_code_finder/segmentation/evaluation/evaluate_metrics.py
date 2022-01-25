f76f440f7b1120cb910797d104e9eb515bc6c746
hendraet <hendrik.raetz@niederjahna.de>
2022-01-25 15:46:26 +0100

    Improves tables in evaluation

--- stylegan_code_finder/segmentation/evaluation/evaluate_metrics.py
+++ stylegan_code_finder/segmentation/evaluation/evaluate_metrics.py
@@ -15,8 +15,10 @@ from segmentation.evaluation.evaluation_utils import preprocess_results, get_tab
 
 if os.environ.get("REMOTE_PYCHARM_DEBUG_SESSION", False):
     import pydevd_pycharm
+
     pydevd_pycharm.settrace('localhost', port=int(os.environ.get("REMOTE_PYCHARM_DEBUG_PORT")),
                             stdoutToServer=True, stderrToServer=True, suspend=False)
+
 
 def print_data_frame(data_frame: pandas.DataFrame, title: str = ""):
     if title != "":
@@ -85,8 +87,7 @@ def print_result_tables(results: dict):
             min_confidence = float(best_results.loc[best_results['class'] == "weighted_avg"]["min_confidence"])
             min_contour_area = int(best_results.loc[best_results['class'] == "weighted_avg"]["min_contour_area"])
             patch_overlap = float(best_results.loc[best_results['class'] == "weighted_avg"]["patch_overlap"])
-            params = f"min_confidence {min_confidence} min_contour_area {min_contour_area} patch_overlap {patch_overlap}"
-
+            best_miou_params = f"min_confidence {min_confidence} min_contour_area {min_contour_area} patch_overlap {patch_overlap}"
 
         print("## Median Results for each Hyperparameter\n")
         best_median_configs = get_best_median_configs(tabular_results, hyperparam_names, score_class_names,
@@ -94,7 +95,14 @@ def print_result_tables(results: dict):
         for hyperparam_name, df in best_median_configs.items():
             print_data_frame(df, title=f"### {hyperparam_name}")
 
-    print_result_for_given_config(params.split(" "), results)
+    print("# Config for best mIoU\n")
+    results = get_result_for_given_config(best_miou_params.split(" "), results)
+    df = get_dataframe_from_results(results)
+    df = df.rename(columns={"iou_weighted_avg": "mIoU", "iou_weighted_text_avg": "mIoU_text_only"}).T
+    print_data_frame(df.head(3))
+
+    print("# All Metrics for best mIoU\n")
+    print_data_frame(df.tail(len(df) - 3))
 
 
 def are_configs_matching(hyperparam_config: dict, run_config: dict) -> bool:
@@ -106,7 +114,7 @@ def are_configs_matching(hyperparam_config: dict, run_config: dict) -> bool:
     return True
 
 
-def print_result_for_given_config(raw_config: List[str], results: dict):
+def get_result_for_given_config(raw_config: List[str], results: dict) -> List[dict]:
     hyperparam_config = dict(zip(raw_config[::2], raw_config[1::2]))
     matching_results = []
     for run in results["runs"]:
@@ -118,22 +126,31 @@ def print_result_for_given_config(raw_config: List[str], results: dict):
                 renamed_results = {f'{score_name}_{k}': v["score"] for k, v in run[score_key].items()}
                 merged_results = {**merged_results, **renamed_results}
             matching_results.append(merged_results)
+    return matching_results
 
+
+def get_dataframe_from_results(matching_results: List[dict]) -> pandas.DataFrame:
+    pandas_ready_dict = defaultdict(list)
+    for result in matching_results:
+        for k, v in result.items():
+            pandas_ready_dict[k].append(v)
+    return pandas.DataFrame(data=pandas_ready_dict)
+
+
+def print_result_for_given_config(raw_config: List[str], results: dict):
+    matching_results = get_result_for_given_config(raw_config, results)
     if len(matching_results) == 0:
         print("No matching config found.")
     else:
-        pandas_ready_dict = defaultdict(list)
-        for result in matching_results:
-            for k, v in result.items():
-                pandas_ready_dict[k].append(v)
-        print_data_frame(pandas.DataFrame(data=pandas_ready_dict))
+        df = get_dataframe_from_results(matching_results)
+        print_data_frame(df)
 
 
 def main(args: argparse.Namespace):
     with open(args.results_path, "r") as results_json:
         results = json.load(results_json)
     preprocess_results(results)
-    
+
     if args.calculate_mean_iou:
         add_mean_iou(results)
 
